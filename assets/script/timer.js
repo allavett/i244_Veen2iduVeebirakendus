82b92675f49f6f3fb4c78129eee3043abcc56a71
Allar Vendla <allarvendla@gmail.com>
2017-02-19 22:20:15 +0200

    Initial commit. Contains main page, css and timer script.

---
+++ assets/script/timer.js
@@ -0,0 +1,36 @@
+/**
+ * Created by AllarVendla on 12.02.2017.
+ */
+
+var date, lastDayOfTheMonth;
+date = new Date;
+
+window.document.addEventListener("DOMContentLoaded", function () {
+    var currentDateElement = document.getElementById("current-date");
+    var daysRemainingElement = document.getElementById("days-remaining");
+    setInterval(function(){updateTime(currentDateElement, daysRemainingElement)}, 1000);
+});
+
+function updateTime(dateElement, daysRemainingElement) {
+    date.setTime(Date.now());
+    dateElement.innerHTML = date.toLocaleString('et-ET', {day: "numeric" ,month: 'long', year: 'numeric'});
+    daysRemainingElement.innerHTML = daysRemainingToLastDayOfTheMonth();
+}
+
+function daysRemainingToLastDayOfTheMonth(){
+    var daysLeft, hoursLeft, minutesLeft, secondsLeft, daysPlural, hoursPlural, minutesPlural, secondsPlural;
+    lastDayOfTheMonth = (lastDayOfTheMonth == undefined || lastDayOfTheMonth <= date) ? new Date(date.getFullYear(),
+            date.getMonth() + 1, 0) : lastDayOfTheMonth;
+    daysLeft = lastDayOfTheMonth.getDate() - date.getDate();
+    hoursLeft = 23 - date.getHours();
+    minutesLeft = 59 - date.getMinutes();
+    secondsLeft = 59 - date.getSeconds();
+    daysPlural = (daysLeft > 1) ? " päeva " : " päev ";
+    hoursPlural = (hoursLeft > 1) ? " tundi ": " tund ";
+    minutesPlural = (minutesLeft > 1) ? " minutit " : " minut ";
+    secondsPlural = (secondsLeft > 1) ? " sekundit " : " sekund ";
+    if (daysLeft > 0) {return daysLeft + daysPlural + " ja " + hoursLeft + hoursPlural}
+    else if (hoursLeft > 0) {return hoursLeft + hoursPlural + " ja " + minutesLeft + minutesPlural}
+    else if (minutesLeft > 0) {return minutesLeft + minutesPlural + " ja " + secondsLeft + secondsPlural}
+    else {"Aeg näidu teatamiseks!"} // <-- Hetkel ei tohiks siia jõuda, kuna algab uus kuu ja siis arvutatakse kuulõpp uuesti
+}
