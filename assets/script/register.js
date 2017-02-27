2bc4a2d749c261a38ef0ae6aee500ed38509823c
Allar Vendla <allarvendla@gmail.com>
2017-02-27 18:43:06 +0200

    Add register js.

---
+++ assets/script/register.js
@@ -0,0 +1,8 @@
+/**
+ * Created by AllarVendla on 27.02.2017.
+ */
+
+window.document.getElementById("register-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("test");
+});
